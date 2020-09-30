cb8777f2d8ca3eeda89b74d91ee8d9be6486467d
Gwathlo <Gwathlo@gwathlo.cat>
2020-09-30 20:58:06 +0200

    Two microservices (RestTemplate and Feign)

---
+++ src/main/java/com/formacionbdi/springboot/app/item/clientes/ProductoClienteRest.java
@@ -0,0 +1,21 @@
+package com.formacionbdi.springboot.app.item.clientes;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.formacionbdi.springboot.app.item.models.Producto;
+
+@FeignClient(name = "servicio-productos", url = "localhost:8001" )
+public interface ProductoClienteRest {
+	
+	@GetMapping("/listar")
+	// mapping del endpoint
+	public List<Producto> listar();
+	
+	@GetMapping("/listar/{id}")
+	Producto obtenerProducto(@PathVariable Long id);
+
+}
---
+++ src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImplFeign.java
@@ -0,0 +1,33 @@
+package com.formacionbdi.springboot.app.item.models.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import com.formacionbdi.springboot.app.item.clientes.ProductoClienteRest;
+import com.formacionbdi.springboot.app.item.models.Item;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+@Primary
+public class ItemServiceImplFeign implements ItemService {
+
+	public ProductoClienteRest productoClienteRest;
+	
+	@Override
+	public List<Item> findAll() {
+		return productoClienteRest.listar().stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
+	}
+
+	@Override
+	public Item findById(Long id, Integer cantidad) {
+		return new Item(productoClienteRest.obtenerProducto(id), cantidad);
+	}
+
+
+
+}
